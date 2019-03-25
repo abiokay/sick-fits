519eb9fc9bc0e0e45741a716d992c08c0e7f7eed
abiokay <koladebiodun@yahoo.com>
2019-03-25 23:52:23 +0100

    added CreateItem component

---
+++ sick-fits/frontend/components/CreateItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql'
+
+class CreateItem extends Component {
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
+
+export default CreateItem;
