169b84dc9a66c3a79ef76f65ab61a4bb7926b625
abiokay <koladebiodun@yahoo.com>
2019-03-28 01:05:25 +0100

    edited DeleteItem component

--- sick-fits/frontend/components/DeleteItem.js
+++ sick-fits/frontend/components/DeleteItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Mutation } from 'react-apollo';
-import gql from 'graphql-tag';
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import { ALL_ITEMS_QUERY } from "./Items";
 
 const DELETE_ITEM_MUTATION = gql`
 	mutation DELETE_ITEM_MUTATION($id: ID!) {
@@ -11,10 +12,42 @@ const DELETE_ITEM_MUTATION = gql`
 `;
 
 class DeleteItem extends Component {
+	update = (cache, payload) => {
+		//update the cache on the client, so it matches the server
+		//1. Read the cache for the remaining items
+		const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+		console.log(data, payload);
+		//2. Filter the deleted item out of the page
+		data.items = data.items.filter(
+			item => item.id !== payload.data.deleteItem.id
+		);
+		//3 load remaining items back to the page
+		cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+	};
 	render() {
 		return (
-			<button>{this.props.children}</button>;
-			)
+			<Mutation
+				mutation={DELETE_ITEM_MUTATION}
+				variables={{ id: this.props.id }}
+				update={this.update}
+			>
+				{(deleteItem, { error }) => (
+					<button
+						onClick={() => {
+							if (
+								confirm(
+									"Are you sure you want to delete this item?"
+								)
+							) {
+								deleteItem();
+							}
+						}}
+					>
+						{this.props.children}
+					</button>
+				)}
+			</Mutation>
+		);
 	}
 }
 
